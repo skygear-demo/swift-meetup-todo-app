48fd536bdf98bb2833ebe67321a0b10eefd1cc16
david90 <david.ng@oursky.com>
2016-09-08 10:24:04 +0800

    Add todo list view controller, implemented basic load record

---
+++ todolist/TodoTableViewController.swift
@@ -0,0 +1,120 @@
+//
+//  TodoTableViewController.swift
+//  todolist
+//
+//  Created by David Ng on 8/9/2016.
+//  Copyright © 2016 SkygearIO. All rights reserved.
+//
+
+import UIKit
+import SKYKit
+
+class TodoTableViewController: UITableViewController {
+
+    let privateDB = SKYContainer.defaultContainer().privateCloudDatabase
+    var objects = [AnyObject]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.updateData()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func updateData() {
+        let query = SKYQuery(recordType: "todo", predicate: NSPredicate(format: "done == false"))
+        let sortDescriptor = NSSortDescriptor(key: "order", ascending: false)
+        query.sortDescriptors = [sortDescriptor]
+        
+        privateDB.performCachedQuery(query) { (results, cached, error) in
+            if (error != nil) {
+                print("error querying todos: \(error)")
+                return
+            }
+            
+            self.objects = results
+            self.tableView.reloadData()
+        }
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return objects.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("todocell", forIndexPath: indexPath)
+
+        // Simple way to dislpay the title to the todo item
+        let object = objects[indexPath.row]
+        cell.textLabel!.text = object.objectForKey!("title") as? String
+
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+
+
+    
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
